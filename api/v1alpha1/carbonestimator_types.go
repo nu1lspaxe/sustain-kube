615038d256a74a81db3f8b110250b7cb277b6228
evanho0315 <evanho0315@gmail.com>
2025-09-25 14:42:43 +0800

    0925 updatev2

--- api/v1alpha1/carbonestimator_types.go
+++ api/v1alpha1/carbonestimator_types.go
@@ -44,7 +44,7 @@ type CarbonEstimatorStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	CarbonIntensity string `json:"carbonIntensity,omitempty"` //碳強度（從 API 拿值，之後再配合comsumption算出emission）
+	CarbonIntensity string `json:"carbonIntensity,omitempty"` // 碳強度（從 API 拿值，之後再配合comsumption算出emission）
 	Consumption     string `json:"consumption,omitempty"`
 	Emission        string `json:"emission,omitempty"`
 	State           string `json:"state,omitempty"`
